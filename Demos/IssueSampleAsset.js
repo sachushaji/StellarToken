19d9d815d14a9e41530482346aa3aad5b3fc4c06
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2019-02-04 19:35:32 +0530

    Adding issue Asset Sample

--- Demos/IssueSampleAsset.js
+++ Demos/IssueSampleAsset.js
@@ -1,46 +1,48 @@
-var StellarSdk = require('stellar-sdk');
+const StellarSdk = require('stellar-sdk');
+
 StellarSdk.Network.useTestNetwork();
-var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
 // Keys for accounts to issue and receive the new asset
-var issuingKeys = StellarSdk.Keypair
+const issuingKeys = StellarSdk.Keypair
   .fromSecret('SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4');
-var receivingKeys = StellarSdk.Keypair
+const receivingKeys = StellarSdk.Keypair
   .fromSecret('SDSAVCRE5JRAI7UFAVLE5IMIZRD6N6WOJUWKY4GFN34LOBEEUS4W2T2D');
 
 // Create an object to represent the new asset
-var astroDollar = new StellarSdk.Asset('AstroDollar', issuingKeys.publicKey());
+const astroDollar = new StellarSdk.Asset('AstroDollar', issuingKeys.publicKey());
 
 // First, the receiving account must trust the asset
 server.loadAccount(receivingKeys.publicKey())
-  .then(function(receiver) {
-    var transaction = new StellarSdk.TransactionBuilder(receiver)
+  .then((receiver) => {
+    const transaction = new StellarSdk.TransactionBuilder(receiver)
       // The `changeTrust` operation creates (or alters) a trustline
       // The `limit` parameter below is optional
       .addOperation(StellarSdk.Operation.changeTrust({
         asset: astroDollar,
-        limit: '1000'
-      }))
+        limit: '1000',
+      })).setTimeout(1000)
       .build();
     transaction.sign(receivingKeys);
     return server.submitTransaction(transaction);
   })
 
   // Second, the issuing account actually sends a payment using the asset
-  .then(function() {
-    return server.loadAccount(issuingKeys.publicKey())
-  })
-  .then(function(issuer) {
-    var transaction = new StellarSdk.TransactionBuilder(issuer)
+  .then(() => server.loadAccount(issuingKeys.publicKey()))
+  .then((issuer) => {
+    const transaction = new StellarSdk.TransactionBuilder(issuer)
       .addOperation(StellarSdk.Operation.payment({
         destination: receivingKeys.publicKey(),
         asset: astroDollar,
-        amount: '10'
-      }))
+        amount: '10',
+      })).setTimeout(1000)
       .build();
     transaction.sign(issuingKeys);
     return server.submitTransaction(transaction);
   })
-  .catch(function(error) {
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
     console.error('Error!', error);
   });
