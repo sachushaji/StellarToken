0db92f891d1d3e66f4a380e280d0c6820322c728
mstars <vellackallintothomas@gmail.com>
2019-02-06 12:27:08 +0530

    Send XLM

--- Demos/AccountCreation.js
+++ Demos/AccountCreation.js
@@ -5,9 +5,10 @@ Stellar.Network.useTestNetwork();
 
 async function createNewAccount() {
   const pair = Stellar.Keypair.random();
-  console.log(`public key = ${pair.publicKey()} private key = ${pair.secret()}`);
+  console.log(`Public key  = ${pair.publicKey()}\nPrivate key = ${pair.secret()}`);
 }
 
 createNewAccount();
+
 // https://medium.com/wearetheledger/exploring-stellar-lumens-development-tutorial-78b4e1c92733
 //https://medium.com/wearetheledger/stellar-escrow-smart-contract-development-4c43ef32ac4b
---
+++ Demos/sendxlm.js
@@ -0,0 +1,84 @@
+// Create, sign, and submit a transaction using JS Stellar SDK.
+
+// Assumes that you have the following items:
+// 1. Secret key of a funded account to be the source account
+// 2. Public key of an existing account as a recipient
+//    These two keys can be created and funded by the friendbot at
+//    https://www.stellar.org/laboratory/ under the heading "Quick Start: Test Account"
+// 3. Access to JS Stellar SDK (https://github.com/stellar/js-stellar-sdk)
+//    either through Node.js or in the browser.
+
+// To use in node, do `npm install stellar-sdk` and uncomment the following line.
+const StellarSdk = require('stellar-sdk');
+
+// This code can be run in the browser at https://www.stellar.org/laboratory/
+// That site exposes a global StellarSdk object you can use.
+// To run this code in the Chrome, open the console tab in the DevTools.
+// The hotkey to open the DevTools console is Ctrl+Shift+J or (Cmd+Opt+J on Mac).
+
+// To use in node, do `npm install stellar-sdk` and uncomment the following line.
+// var StellarSdk = require('stellar-sdk');
+
+// The source account is the account we will be signing and sending from.
+var sourceSecretKey = 'SBRFUKERGXPRJEWPLJSIKRIG3O45BGAKGVKNU7KW2YTZ7CEZYECOHLEN';
+
+// Derive Keypair object and public key (that starts with a G) from the secret
+var sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
+var sourcePublicKey = sourceKeypair.publicKey();
+
+var receiverPublicKey = 'GDTBPYZLEXIHUFMCYUHV4LBT6VJGJQOVYPCZMMCKUKSZ7OARBFUDWJG6';
+
+// Configure StellarSdk to talk to the horizon instance hosted by Stellar.org
+// To use the live network, set the hostname to 'horizon.stellar.org'
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+// Uncomment the following line to build transactions for the live network. Be
+// sure to also change the horizon hostname.
+// StellarSdk.Network.usePublicNetwork();
+StellarSdk.Network.useTestNetwork();
+
+// Transactions require a valid sequence number that is specific to this account.
+// We can fetch the current sequence number for the source account from Horizon.
+server.loadAccount(sourcePublicKey)
+  .then(function(account) {
+    var transaction = new StellarSdk.TransactionBuilder(account)
+      // Add a payment operation to the transaction
+      .addOperation(StellarSdk.Operation.payment({
+        destination: receiverPublicKey,
+        // The term native asset refers to lumens
+        asset: StellarSdk.Asset.native(),
+        // Specify 1000 lumens. Lumens are divisible to seven digits past
+        // the decimal. They are represented in JS Stellar SDK in string format
+        // to avoid errors from the use of the JavaScript Number data structure.
+        amount: '1000',
+      }))
+      // Uncomment to add a memo (https://www.stellar.org/developers/learn/concepts/transactions.html)
+      // .addMemo(StellarSdk.Memo.text('Hello world!'))
+      .setTimeout(1000)
+      .build();
+
+    // Sign this transaction with the secret key
+    // NOTE: signing is transaction is network specific. Test network transactions
+    // won't work in the public network. To switch networks, use the Network object
+    // as explained above (look for StellarSdk.Network).
+    transaction.sign(sourceKeypair);
+
+    // Let's see the XDR (encoded in base64) of the transaction we just built
+    console.log(transaction.toEnvelope().toXDR('base64'));
+
+    // Submit the transaction to the Horizon server. The Horizon server will then
+    // submit the transaction into the network for us.
+    server.submitTransaction(transaction)
+      .then(function(transactionResult) {
+        console.log(JSON.stringify(transactionResult, null, 2));
+        console.log('\nSuccess! View the transaction at: ');
+        console.log(transactionResult._links.transaction.href);
+      })
+      .catch(function(err) {
+        console.log('An error has occured:');
+        console.log(err);
+      });
+  })
+  .catch(function(e) {
+    console.error(e);
+  });
